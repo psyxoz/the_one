71a34dbe196ca5d626b859614f7cc5ac13309ed7
Sergey Homenko <xoma.serg@gmail.com>
2017-11-30 16:50:30 +0200

    Handle Loopholes

---
+++ lib/the_one/routes/loopholes.rb
@@ -0,0 +1,40 @@
+module TheOne::Routes
+  class Loopholes < Base
+    def routes
+      grouped_routes.each do |_, items|
+        route = {
+          start_node: items.first[:start_node],
+          end_node: items.last[:end_node],
+          start_time: Time.parse(items.first[:time]).strftime('%FT%T'),
+          end_time: Time.parse(items.last[:time]).strftime('%FT%T')
+        }
+
+        yield(route)
+      end
+    end
+
+    private
+
+    def node_pairs
+      @node_pairs ||= {}.tap do |rows|
+        MultiJson.load(files['node_pairs.json'], symbolize_keys: true)['node_pairs'].each do |row|
+          rows[row[:id]] = row
+        end
+      end
+    end
+
+    def grouped_routes
+      @grouped_routes ||= Hash.new { |h,k| h[k] = [] }.tap do |rows|
+        MultiJson.load(files['routes.json'], symbolize_keys: true)['routes'].each do |row|
+          node_pair = node_pairs[row[:node_pair_id]]
+          next unless node_pair
+
+          rows[row[:route_id]] << row.merge(
+            start_node: node_pair[:start_node],
+            end_node: node_pair[:end_node]
+          )
+        end
+      end
+    end
+  end
+end
--- lib/the_one/the_one.rb
+++ lib/the_one/the_one.rb
@@ -9,10 +9,11 @@ require_relative 'helpers'
 require_relative 'routes/base'
 require_relative 'routes/sentinels'
 require_relative 'routes/sniffers'
+require_relative 'routes/loopholes'
 
 module TheOne
   def self.perform
-    [ Routes::Sentinels, Routes::Sniffers ].each do |klass|
+    [ Routes::Sentinels, Routes::Sniffers, Routes::Loopholes ].each do |klass|
       instance = klass.new
       instance.routes do |route|
         instance.upload(route)
